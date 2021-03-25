81260b34e4ae0134eb4114222637d1fdee98d650
onepro19 <w.settiawan@gmail.com>
2021-03-25 17:11:58 +0700

    fitur pretest ready

--- script/pretest.js
+++ script/pretest.js
@@ -1,10 +1,14 @@
 import { dataSoalPreTest } from "./soal.js";
 let nomor_soal = 1;
+let pilihan1, pilihan2, pilihan3, pilihan4, pilihan5;
+let soalContainerElement, boxElement;
+let temp_jawaban = "";
+let skor = 0;
 const getButton = () => {
-  const btnHide = document.querySelector("#hideBtn");
-  const boxElement = document.querySelector(".box-dialog-container");
+  boxElement = document.querySelector(".box-dialog-container");
   const countdownElement = document.querySelector(".countdown");
-  const soalContainerElement = document.querySelector(".soalContainer");
+  soalContainerElement = document.querySelector(".soalContainer");
+  const btnHide = document.querySelector("#hideBtn");
   btnHide.addEventListener("click", () => {
     boxElement.classList.add("hide");
     let angka = 3;
@@ -27,33 +31,104 @@ const getSoal = (nomor) => {
 
 const showSoal = (nomor) => {
   const soal = document.querySelector(".pertanyaan");
-  const pilihan1 = document.querySelector("#pilihan1");
-  const pilihan2 = document.querySelector("#pilihan2");
-  const pilihan3 = document.querySelector("#pilihan3");
-  const pilihan4 = document.querySelector("#pilihan4");
-  const pilihan5 = document.querySelector("#pilihan5");
+  pilihan1 = document.querySelector("#pilihan1");
+  pilihan2 = document.querySelector("#pilihan2");
+  pilihan3 = document.querySelector("#pilihan3");
+  pilihan4 = document.querySelector("#pilihan4");
+  pilihan5 = document.querySelector("#pilihan5");
   const btnNext = document.querySelector("#next");
   const btnSelesai = document.querySelector("#selesai");
 
-  if (nomor >= 10) {
+  if (nomor > 20) {
     btnNext.classList.add("hide");
     btnSelesai.classList.remove("hide");
+    soalContainerElement.classList.add("hide");
+  } else {
+    soal.innerHTML = `${dataSoalPreTest[nomor - 1].soal}`;
+    pilihan1.innerHTML = `${dataSoalPreTest[nomor - 1].pilihan1}`;
+    pilihan2.innerHTML = `${dataSoalPreTest[nomor - 1].pilihan2}`;
+    pilihan3.innerHTML = `${dataSoalPreTest[nomor - 1].pilihan3}`;
+    pilihan4.innerHTML = `${dataSoalPreTest[nomor - 1].pilihan4}`;
+    pilihan5.innerHTML = `${dataSoalPreTest[nomor - 1].pilihan5}`;
+
+    btnNext.addEventListener("click", handleNext);
+    btnSelesai.addEventListener("click", handleSelesai);
+    pilihan1.addEventListener("click", handlePilihan1);
+    pilihan2.addEventListener("click", handlePilihan2);
+    pilihan3.addEventListener("click", handlePilihan3);
+    pilihan4.addEventListener("click", handlePilihan4);
+    pilihan5.addEventListener("click", handlePilihan5);
   }
+};
 
-  soal.innerHTML = `${dataSoalPreTest[nomor - 1].soal}`;
-  pilihan1.innerHTML = `${dataSoalPreTest[nomor - 1].pilihan1}`;
-  pilihan2.innerHTML = `${dataSoalPreTest[nomor - 1].pilihan2}`;
-  pilihan3.innerHTML = `${dataSoalPreTest[nomor - 1].pilihan3}`;
-  pilihan4.innerHTML = `${dataSoalPreTest[nomor - 1].pilihan4}`;
-  pilihan5.innerHTML = `${dataSoalPreTest[nomor - 1].pilihan5}`;
-
-  btnNext.addEventListener("click", handleNext);
+const handleSelesai = () => {
+  boxElement.classList.remove("hide");
+  boxElement.innerHTML = `<h2>${skor}</h2>`;
 };
 
 const handleNext = () => {
+  pilihan1.classList.add("gradient");
+  pilihan2.classList.add("gradient");
+  pilihan3.classList.add("gradient");
+  pilihan4.classList.add("gradient");
+  pilihan5.classList.add("gradient");
+  if (temp_jawaban == dataSoalPreTest[nomor_soal - 1].jawaban) skor = skor + 5;
+  console.log(skor);
   nomor_soal = nomor_soal + 1;
-  console.log(nomor_soal);
+  tampilProgress(nomor_soal);
   getSoal(nomor_soal);
+};
+
+const handlePilihan1 = () => {
+  pilihan1.classList.remove("gradient");
+  pilihan2.classList.add("gradient");
+  pilihan3.classList.add("gradient");
+  pilihan4.classList.add("gradient");
+  pilihan5.classList.add("gradient");
+  temp_jawaban = dataSoalPreTest[nomor_soal - 1].pilihan1;
+};
+const handlePilihan2 = () => {
+  pilihan1.classList.add("gradient");
+  pilihan2.classList.remove("gradient");
+  pilihan3.classList.add("gradient");
+  pilihan4.classList.add("gradient");
+  pilihan5.classList.add("gradient");
+  temp_jawaban = dataSoalPreTest[nomor_soal - 1].pilihan2;
+};
+const handlePilihan3 = () => {
+  pilihan1.classList.add("gradient");
+  pilihan2.classList.add("gradient");
+  pilihan3.classList.remove("gradient");
+  pilihan4.classList.add("gradient");
+  pilihan5.classList.add("gradient");
+  temp_jawaban = dataSoalPreTest[nomor_soal - 1].pilihan3;
+};
+const handlePilihan4 = () => {
+  pilihan1.classList.add("gradient");
+  pilihan2.classList.add("gradient");
+  pilihan3.classList.add("gradient");
+  pilihan4.classList.remove("gradient");
+  pilihan5.classList.add("gradient");
+  temp_jawaban = dataSoalPreTest[nomor_soal - 1].pilihan4;
+};
+const handlePilihan5 = () => {
+  pilihan1.classList.add("gradient");
+  pilihan2.classList.add("gradient");
+  pilihan3.classList.add("gradient");
+  pilihan4.classList.add("gradient");
+  pilihan5.classList.remove("gradient");
+  temp_jawaban = dataSoalPreTest[nomor_soal - 1].pilihan5;
+};
+
+const tampilProgress = (progress) => {
+  const progressBar = document.querySelector("#progressBar");
+  if (progress <= 20) {
+    progressBar.innerHTML = `${progress}/20`;
+    progressBar.style.width = `${(progress / 20) * 100}%`;
+  } else {
+    progressBar.innerHTML = `Test Selesai`;
+    progressBar.style.width = `100%`;
+  }
 };
 
 export { getButton };
---
+++ test.cpp
@@ -0,0 +1,11 @@
+int jumlah_bebek = 10;
+
+if(jumlah_bebek < 10){
+  jumlah_bebek = jumlah_bebek *10;
+}else{
+  jumlah_bebek = jumlah_bebek+2;
+}
+
+if(jumlah_bebek < 15){
+  jumlah_bebek = jumlah_bebek +5;
+}
